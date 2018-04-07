7f661f0ea833d0509b2fa2fe530287df889cf97e
stevenasher <36062665+stevenasher@users.noreply.github.com>
2018-04-07 16:31:54 +0800

    Update and rename footer.html to footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Frameset</title>
+    <title>My Connection</title>
     <style type="text/css">
         body{
             font-family: Verdana, Arial, sans-serif;
@@ -33,18 +33,12 @@
 <body>
 <table>
 	<tr>
-		<td><a href="home.html" target="content">Back to home</a> | MC &copy; 2018 |</td>
+		<td><a href="home.php" target="content">Back to home</a> | MC &copy; 2018 |</td>
 		<td><a href="https://www.facebook.com/brandon.dwite" target="_blank"><img src="https://media3.picsearch.com/is?bmkyMhW1qlJnFNUcw6oC1ETRz03SSxbAHnofFStLBD4&height=341" class="icon"></a></td>
 		<td><a href="https://www.facebook.com/brandon.dwite" target="_blank">brandon.dwite</a> |</td>
 		<td><a href="https://www.facebook.com/brandon.dwite" target="_blank"><img src="http://icons.iconarchive.com/icons/dakirby309/windows-8-metro/256/Web-Gmail-Metro-icon.png" class="icon"></a></td>
 		<td><a href="mailto:bdscobacha@gmail.com?subject=Someone%20wants%20to%20reach%20you%20on%20gmail.&body=This%20is%20a%20generated%20email.">bdscobacha@gmail.com</a></td>
 	</tr>
 </table>
-<p>
-	
-    
-    
-    
-</p>
 </body>
 </html>
