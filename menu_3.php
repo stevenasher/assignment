2023c34c23f8017398e4b25223077a0ef22841f9
stevenasher <36062665+stevenasher@users.noreply.github.com>
2018-04-07 16:39:49 +0800

    Update and rename menu_3.html to menu_3.php

--- menu_3.php
+++ menu_3.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Frameset</title>
+    <title>My Connection</title>
     <style type="text/css">
         body{
             font-family: Verdana, Arial, sans-serif;
@@ -24,9 +24,9 @@
 <body>
 <h3>Other Relatives</h3>
 <hr>
-<p><a href="lorena.html" target="content">Lorena Cobacha Canico</a></p>
-<p><a href="reinadel.html" target="content">Reinadel Cobacha Canico</a></p>
-<p><a href="reindel.html" target="content">Reindel Cobacha Canico</a></p>
-<p><a href="rey.html" target="content">Rey Cobacha Canico Jr.</a></p>
+<p><a href="lorena.php" target="content">Lorena Cobacha Canico</a></p>
+<p><a href="reinadel.php" target="content">Reinadel Cobacha Canico</a></p>
+<p><a href="reindel.php" target="content">Reindel Cobacha Canico</a></p>
+<p><a href="rey.php" target="content">Rey Cobacha Canico Jr.</a></p>
 </body>
 </html>
