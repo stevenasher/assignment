8dec6f2fa24605dadc69446ed592e7f51b9bbd41
stevenasher <36062665+stevenasher@users.noreply.github.com>
2018-04-07 16:35:37 +0800

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <link rel="icon" href="http://static1.squarespace.com/static/508957eae4b0f50834dfa92f/t/57a202f4725e256f85ef8af1/1493834741326/" sizes="16x16">
+    <title>My Connection</title>
+</head>
+<frameset rows="130,*,70" frameborder="0" border="0" framespacing="0">
+    <frame name="topNav" src="top_nav.php">
+    <frameset cols="230,*" frameborder="0" border="0" framespacing="0">
+        <frame name="menu" src="menu_1.php" marginheight="0" marginwidth="0" scrolling="auto" noresize>
+        <frame name="content" src="home.php" marginheight="0" marginwidth="0" scrolling="auto" noresize>
+    </frameset>
+    <frame name="footer" src="footer.php">
+    <noframes>
+</frameset>
+<body>
+
+</body>
+</html>
