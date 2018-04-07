b2e59a453279ffb92c02d795315a3aa7a0807d4c
stevenasher <36062665+stevenasher@users.noreply.github.com>
2018-04-07 16:34:13 +0800

    Update and rename home.html to home.php

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Frameset</title>
+    <title>My Connection</title>
     <style type="text/css">
         body{
             font-family: Verdana, Arial, sans-serif;
@@ -51,11 +51,11 @@
 	<tr>
 		<td colspan="2">
 			<img src="https://scontent.fceb1-1.fna.fbcdn.net/v/t1.0-9/25594103_310967649409819_8187660609410475732_n.jpg?_nc_cat=0&oh=9558902f771fdc66bbb51404cd698538&oe=5B376EA5" class="propic">
-			<p><a href="father.html" target="content">Manuel Lumpas Cobacha Jr.</a></p>
+			<p><a href="father.php" target="content">Manuel Lumpas Cobacha Jr.</a></p>
 		</td>
 		<td colspan="2">
 			<img src="https://scontent.fceb1-1.fna.fbcdn.net/v/t1.0-9/15966142_160000444486308_6487669232862306533_n.jpg?_nc_cat=0&oh=ce332717115d2e47f1cfd0b6cfd7741d&oe=5B5FFFE7" class="propic">
-			<p><a href="mother.html" target="content">Josefina Sugbo Cobacha</a></p>
+			<p><a href="mother.php" target="content">Josefina Sugbo Cobacha</a></p>
 		</td>
 	</tr>
 	<tr>
@@ -68,19 +68,19 @@
 	<tr>
 		<td>
 			<img src="https://scontent.fceb1-1.fna.fbcdn.net/v/t1.0-9/18447534_1494123330639413_3949757283560142932_n.jpg?_nc_cat=0&oh=3d9213692f107428f9e9a18cdaceab07&oe=5B6EEC62" class="propic">
-			<p><a href="brandon.html" target="content">Brandon Dwite Sugbo Cobacha</a></p>
+			<p><a href="brandon.php" target="content">Brandon Dwite Sugbo Cobacha</a></p>
 		</td>
 		<td>
 			<img src="https://scontent.fceb1-1.fna.fbcdn.net/v/t1.0-9/14962582_452765644893651_739333333121910751_n.jpg?_nc_cat=0&oh=945e32698175996b82ebc91aa40f4e58&oe=5B666CB2" class="propic">
-			<p><a href="bairon.html" target="content">Bairon Brix Sugbo Cobacha</a></p>
+			<p><a href="bairon.php" target="content">Bairon Brix Sugbo Cobacha</a></p>
 		</td>
 		<td>
 			<img src="https://scontent.fceb1-1.fna.fbcdn.net/v/t1.0-9/25660232_101809673954176_1248184067780125441_n.jpg?_nc_cat=0&oh=90a9e466ca17069433bed8b9f65cbe7e&oe=5B3377B5" class="propic">
-			<p><a href="bryl.html" target="content">Bryl Niño Sugbo Cobacha</a></p>
+			<p><a href="bryl.php" target="content">Bryl Niño Sugbo Cobacha</a></p>
 		</td>
 		<td>
 			<img src="https://scontent.fceb1-1.fna.fbcdn.net/v/t1.0-9/17308702_205787979907554_8668165471533262350_n.jpg?_nc_cat=0&oh=cd8ad57c123bdf9b32763f0610069b9d&oe=5B6119CF" class="propic">
-			<p><a href="brixel.html" target="content">Brixel John Sugbo Cobacha</a></p>
+			<p><a href="brixel.php" target="content">Brixel John Sugbo Cobacha</a></p>
 		</td>
 	</tr>
 </table>
@@ -89,10 +89,10 @@
 <br>
 <h3 id="relatives">Other Relatives</h3>
 <ul>
-    <li><a href="lorena.html" target="content">Lorena Cobacha Canico</a></li>
-    <li><a href="reinadel.html" target="content">Reinadel Cobacha Canico</a></li>
-    <li><a href="reindel.html" target="content">Reindel Cobacha Canico</a></li>
-	<li><a href="rey.html" target="content">Rey Cobacha Canico Jr.</a></li>
+    <li><a href="lorena.php" target="content">Lorena Cobacha Canico</a></li>
+    <li><a href="reinadel.php" target="content">Reinadel Cobacha Canico</a></li>
+    <li><a href="reindel.php" target="content">Reindel Cobacha Canico</a></li>
+    <li><a href="rey.php" target="content">Rey Cobacha Canico Jr.</a></li>
 </ul>
 </body>
 </html>
